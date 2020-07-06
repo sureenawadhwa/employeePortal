89538f45b33ab8affacf0412c9a1a5b8662eb840
sureenawadhwa <sureenawadhwa@Sureenas-MacBook-Air.local>
2020-07-07 01:05:49 +0530

    first commit

---
+++ src/test/java/com/sureena_wadhwa/employeePortal/EmployeePortalApplicationTests.java
@@ -0,0 +1,14 @@
+package com.sureena_wadhwa.employeePortal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class EmployeePortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
